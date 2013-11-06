1df7955df1c489add89eccff16d5723e1333c15c
Marinos MREL <marinosbern@yahoo.com>
2013-11-06 14:26:16 -0500

    Adding trivial comm protocol Start reading C Position input and use it as a safeguard Toque origin is 2.5V (130 duty cycle

--- arduino/virtual_spring/virtual_spring.ino
+++ arduino/virtual_spring/virtual_spring.ino
@@ -2,16 +2,18 @@
 
 int posA = A0;
 int posB = A1;
+int posC = A2;
 
-int torquePin=10;
-int velocityPin=10;
+int torquePin=9;
+int velocityPin=9;
 
 const int greyCodeConversion[]={0,1,3,2};
 
-long lastValue=0;
+int lastValue=0;
 long pos=0;
 
-long revolutions=0;
+int lastCValue=0;
+int realRevolutions=0;
 
 unsigned long lastTriggerMillis=0;
 int triggerIntervalMillis=2000;
@@ -21,55 +23,66 @@ int errors=0;
 int velocity=0;
 
 boolean serial=true;
-boolean autonomous=false;
+boolean autonomous=true;
+
+int rotationDirection=0;
+
+int zeroTorque=129;
+
+int stiffness=10;
 
 void setup()
 {
   if(serial) Serial.begin(9600);
   pinMode(posA, INPUT_PULLUP);
   pinMode(posB, INPUT_PULLUP);
+  pinMode(posC, INPUT_PULLUP);
   pinMode(torquePin, OUTPUT);
+  analogWrite(torquePin, zeroTorque);
 }
 
 void loop()
 {
   updatePosition();
   if(autonomous) applyTorque();
-  if(serial) performPeriodicCommunication();
-  if(serial &&! autonomous) writeVelocity();
+  if(serial) performOutput();
+  if(serial) processSerialInput();
 }
 
 void applyTorque()
 {
-  int torque=revolutions ? 0:max(0,min(pos/600, 50));
+  int torque=abs(realRevolutions)>5 ? zeroTorque:max(80,min(zeroTorque-((pos)/stiffness), 180));
   analogWrite(torquePin, torque);
 }
 
 void updatePosition()
 {
+  //A&B
   int newValue=readValue();
-  switch(newValue-lastValue)
+  if(newValue!=lastValue)
   {
-    case 1: //rising edge
+    if(newValue==2 && lastValue==1)
+    {
       pos++;
-      break;
-    case -1:
+      rotationDirection=1;
+    }
+    else if(newValue==0 && lastValue==1)
+    {
       pos--;
-      break;
-    case 0:
-      break;
-    default:
-      errors++;
-      break;
-    
+      rotationDirection=-1;
+    }
   }
   lastValue=newValue;
   
-  if(pos/6000!=0)
+  //C
+  int newCValue=digitalRead(posC);
+  if(newCValue!=lastCValue && newCValue)
   {
-    revolutions+=pos/6000;
-    pos=pos%6000;
+    //pos=0;
+    realRevolutions+=rotationDirection;
+    //realRevolutions++;
   }
+  lastCValue=newCValue;
 }
 
 int readValue()
@@ -79,24 +92,40 @@ int readValue()
   return greyCodeConversion[greyCode];
 }
 
-void writeVelocity()
+void processSerialInput()
 {
  if(Serial.available()>0)
   {
-     int incomingDutyCycle = Serial.parseInt();
-     Serial.read();  //newline
+    char x=Serial.read();
+    if(x=='v')
+    {
+      int incomingDutyCycle = Serial.parseInt();
+     
      analogWrite(velocityPin, incomingDutyCycle);
+    }
+    else if(x=='r')
+    {
+      realRevolutions=0;
+      pos=0;
+    }
+    else if(x=='s')
+    {
+      stiffness = Serial.parseInt();
+    }
+     Serial.read();  //newline
   } 
 }
 
-void performPeriodicCommunication()
+void performOutput()
 {  
   unsigned long currentMillis=millis();
   if(currentMillis-lastTriggerMillis>triggerIntervalMillis)
   {
     Serial.print(pos);
-    Serial.print(" r:");
-    Serial.print(revolutions);
+    Serial.print(" R:");
+    Serial.print(realRevolutions);
+    Serial.print("\n");
+    Serial.print(rotationDirection);
     Serial.print("\n");
     lastTriggerMillis=currentMillis;
   }
