d97da507989f25b22daf58ea116b8e49028718b1
Marinos MREL <marinosbern@yahoo.com>
2014-02-24 18:45:31 -0500

    -Adding system ID code

---
+++ System ID/System ID/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace System_ID
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PortReader pr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            slider1.ValueChanged += slider1_ValueChanged;
+
+            InitializePortReader();
+        }
+
+        private void InitializePortReader()
+        {
+            try
+            {
+                pr = new PortReader();
+                pr.NewString += new EventHandler<PortEventArgs>(portReader_NewString);
+                Thread t = new Thread(pr.loop);
+                //t.IsBackground = true;
+                t.Start();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Could not connect to Arduino");
+                pr = null;
+            }
+
+        }
+
+        void portReader_NewString(object sender, PortEventArgs e)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                amplitudeLabel.Content = e.Data;
+            }));
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Write(string.Format("m{0}", slider1.Value));
+        }
+
+        private void Write(string data)
+        {
+            pr.Write(data);
+            Console.WriteLine(data);
+        }
+    }
+}
